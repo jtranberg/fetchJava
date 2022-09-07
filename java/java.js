2de177955a63972cbbfca4b90ede760bb9b6ff2e
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-06 18:52:31 -0700

    second refactor using  .this

--- java/java.js
+++ java/java.js
@@ -1,32 +1,32 @@
-fetch('https://swapi.dev/api/planets/')
-.then((response) => {
+const checkStatusAndParse = (response) => {
     if(!response.ok)
     throw new Error(`Status Code Error: ${response.status}`);
 
-    return response.json();
-})
-.then((data) => {
-    console.log('fetched first ten planets');
-    for(let planet of data.results) {
-        console.log(planet.name);
-    }
-    const nextURL = data.next;
-    return fetch(nextURL); 
-})
-.then((response) => {
-    if (!response.ok)
-    throw new Error(`Satus Code Error: ${response.status}`);
+ return response.json();
+}
 
-    return response.json();
-})
-.then((data) => {
-    console.log('fetched next 10 planets');
+const printPlanets = (data) => {
+console.log('loaded 10 more planets...');
     for(let planet of data.results) {
         console.log(planet.name);
-    }
-    
-})
+}
+return Promise.resolve(data.next)
+};
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+  return fetch(url);
+};
+
+fetchNextPlanets()
+.then(checkStatusAndParse)
+.then(printPlanets)
+.then(fetchNextPlanets)
+.then(checkStatusAndParse)
+.then(printPlanets)
+.then(fetchNextPlanets)
+.then(checkStatusAndParse)
+.then(printPlanets)  
 .catch((err) => {
  console.log('something wrong');
  console.log(err);
 });
+
